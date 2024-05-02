998425294caf207fa63dacf5c21c0af1796536a1
mertugral <ertugralmert@gmail.com>
2024-05-02 17:23:41 +0300

    Hibertane -> CRUD Create, Read, Update, Delete, Search, List...

---
+++ src/main/java/com/mert/Runner.java
@@ -0,0 +1,13 @@
+package com.mert;
+
+public class Runner {
+
+    /**
+     * Öncelikle depencies kısmında tanımlama yaptık bunlar:
+     * Hibertane
+     * Postgre sql
+     * Lombok  bunları ekledik. Maven sitesinden alınmış halleri.
+     * Sonrasında META-INF ve persistence.xml oluşturup gerekli config dosyasını oluşturduk.
+     */
+    // Şimdi gerekli kullanılabilir kısmını halledeceğiz
+}
---
+++ src/main/java/com/mert/repository/Repository.java
@@ -0,0 +1,168 @@
+package com.mert.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Repository<T, ID> implements IRepository<T, ID> {
+
+    private final EntityManagerFactory emf;
+    private EntityManager em;
+    private  T t;
+
+    public Repository(T entity){
+        emf = Persistence.createEntityManagerFactory("CRM");
+        em = emf.createEntityManager();
+        this.t = entity;
+    }
+
+    private void openSession(){
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+    }
+
+    private void closeSession(){
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    private void rollback(){
+        em.getTransaction().rollback();
+    }
+    @Override
+    public T save(T entity) {
+        openSession();
+        em.persist(entity);
+        closeSession();
+        return entity;
+    }
+
+    @Override
+    public Iterable<T> saveAll(Iterable<T> entities) {
+        try{
+            openSession();
+            entities.forEach(em::persist);
+        }catch (Exception e){
+            rollback();
+        }
+        return entities;
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery =(CriteriaQuery<T>) criteriaBuilder.createQuery(t.getClass());
+        Root<T> root =(Root<T>) criteriaQuery.from(t.getClass());
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"),id));
+        T result;
+        try{
+            result = em.createQuery(criteriaQuery).getSingleResult();
+            return Optional.of(result);
+        }catch (NoResultException exception){
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public boolean existsById(ID id) {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery =(CriteriaQuery<T>) criteriaBuilder.createQuery(t.getClass());
+        Root<T> root = (Root<T>) criteriaQuery.from(t.getClass());
+       criteriaQuery.select(root);
+       criteriaQuery.where(criteriaBuilder.equal(root.get("id"),id));
+       try{
+           em.createQuery(criteriaQuery).getResultList();
+           return true;
+       }catch (NoResultException exception){
+           return false;
+       }
+    }
+
+    @Override
+    public List<T> findAll() {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = (CriteriaQuery<T>) criteriaBuilder.createQuery(t.getClass());
+        Root<T> root = (Root<T>) criteriaQuery.from(t.getClass());
+        criteriaQuery.select(root);
+        return em.createQuery(criteriaQuery).getResultList();
+    }
+
+    @Override
+    public List<T> findByColumnAndValue(String columnName, Object value) {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = (CriteriaQuery<T>) criteriaBuilder.createQuery(t.getClass());
+        Root<T> root =(Root<T>) criteriaQuery.from(t.getClass());
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.equal(root.get(columnName),value));
+        return em.createQuery(criteriaQuery).getResultList();
+    }
+
+    @Override
+    public void deleteById(ID id) {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = (CriteriaQuery<T>) criteriaBuilder.createQuery(t.getClass());
+        Root<T> root = (Root<T>) criteriaQuery.from(t.getClass());
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"),id));
+        T removeElement;
+        try{
+            removeElement = em.createQuery(criteriaQuery).getSingleResult();
+        }catch (NoResultException exception){
+            removeElement = null;
+        }
+        try {
+            openSession();
+            em.remove(removeElement);
+            closeSession();
+        }catch (Exception exception){
+            if(em.isOpen()){
+                rollback();
+            }
+        }
+
+    }
+
+    @Override
+    public List<T> findAllByEntity(T entity) {
+        List<T> result;
+        Class<?> clazz = entity.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = (CriteriaQuery<T>) criteriaBuilder.createQuery(t.getClass());
+        Root<T> root = (Root<T>) criteriaQuery.from(t.getClass());
+        criteriaQuery.select(root);
+
+        List<Predicate> predicateList = new ArrayList<>();
+        for(int i =1; i<fields.length; i++){
+            fields[i].setAccessible(true);
+            try{
+                String column = fields[i].getName();
+                Object value = fields[i].get(entity);
+                if(value != null){
+                    if(value instanceof String){
+                        predicateList.add(criteriaBuilder.like(root.get(column),"%"+value+"%"));
+                    }else{
+                        predicateList.add(criteriaBuilder.equal(root.get(column),value));
+                    }
+                }
+
+            }catch (Exception e){
+                System.out.println("Beklenmeyen bir hata oluştu: "+ e);
+            }
+        }
+        criteriaQuery.where(predicateList.toArray(new Predicate[]{}));
+        result = em.createQuery(criteriaQuery).getResultList();
+        return result;
+    }
+}
